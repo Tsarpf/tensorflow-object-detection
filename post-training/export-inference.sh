cdf28058d571e278190bb1b145d825a4c98334a9
Teemu Sarapisto <tsarpf@gmail.com>
2019-01-13 13:59:44 +0200

    Add scripts for exporting .pb from model checkpoints

--- post-training/export-inference.sh
+++ post-training/export-inference.sh
@@ -1,11 +1,5 @@
-
-python setup.py build
-
-python setup.py install
-
-python object_detection/export_inference_graph.py \
+python ./export-inference-graph.py \
     --input_type image_tensor \
-    --pipeline_config_path ./pentect.config \
-    --trained_checkpoint_prefix ./ready-model/model.ckpt-131448 \
-    --output_directory exported_graphs
-
+    --pipeline_config_path ../training/dist/training.config \
+    --trained_checkpoint_prefix ../model-checkpoints/model.ckpt-12410 \
+    --output_directory exported-graphs
